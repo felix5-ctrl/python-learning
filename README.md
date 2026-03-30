e56d5762f15d276082adb58465ad6f54a00c0e7a
felix5 <felix.lassnig5@gmail.com>
2026-03-30 21:50:13 +0200

    added calculator.py, first lesson 2/3

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 # as of now consisting of a CS50P course work along repository
 #
 # Status: Week 1 CS50p Lection 0, about halfway done dtg 290326
+
+#dtg 300326 interactive mode
---
+++ calculator.py
@@ -0,0 +1,13 @@
+x = float(input("What's x?"))
+y = float(input("What's y?"))
+
+z = round(x + y)
+
+print(z)
+
+#input() by default is always a string, redefining necessary
+#int() acts as conversion function into a whole number
+#float() decimal numbers
+#round(number[, ndigits])
+
+#https://youtu.be/JP7ITIXGpHk?t=4836 last moment in lection 0
